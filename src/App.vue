e175c372b7ab0facf4d12a4b983b0c8bad4068c0
Philippe <Philippe@NewAirdPhilippe.lan>
2025-02-21 15:16:38 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+// import HelloWorld from './components/HelloWorld.vue'
+// import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <header></header>
+
+  <main>
+    <p>hello Word</p>
+  </main>
+</template>
+
+<style scoped></style>
